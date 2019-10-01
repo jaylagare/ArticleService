b6950a3744a95e3824a13e9b46eaa137adfe564c
Jay Lagare <jaylagare@gmail.com>
2019-10-01 22:20:44 +0800

    Fix CORS setting to allow all.

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '5mb', parameterLimit: 50
 // app.use(app.router);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
